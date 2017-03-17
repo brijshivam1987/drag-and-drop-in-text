b5c41ca6ffad861c84521c1cf1a80fbe17ca538d
Brijesh Patel <brij_shivam1987@yahoo.com>
2017-03-17 18:15:13 +0530

    Update code
    Add newly uploaded js for device touch issue

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/jquery-ui.js"></script>
         <script type="text/javascript" src="js/jquery.ui.touch-punch.min.js"></script>
+        <script type="text/javascript" src="js/touche.min.js"></script>
         
         <style type="text/css">
             /* draggable targets */
